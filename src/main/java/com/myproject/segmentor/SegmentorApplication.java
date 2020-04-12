c0d022c8a758bcfdd9d63d049eabe381ad6eb3d4
Aniket Mohapatra <getaniket111@gmail.com>
2020-04-12 13:42:22 +0200

    Initial Commit

---
+++ src/main/java/com/myproject/segmentor/SegmentorApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.segmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegmentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/segmentor/SegmentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.segmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
